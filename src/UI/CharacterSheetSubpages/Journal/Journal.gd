f93fabb6d4d14b263bcf557edb99ec8676cbcebd
Michele T Da Silva <michele.tdasilva@gmail.com>
2025-08-25 19:19:20 -0700

    Added a safety button to removing journal entries

--- src/UI/CharacterSheetSubpages/Journal/Journal.gd
+++ src/UI/CharacterSheetSubpages/Journal/Journal.gd
@@ -29,14 +29,14 @@ func _on_NewEntry_button_up() -> void:
 	var cur_date = Time.get_date_string_from_system()
 	var new_entry = journal_entry_template.instantiate()
 	new_entry.size_flags_vertical = new_entry.SIZE_EXPAND_FILL
-	var entry_IRL_timestamp = new_entry.get_child(0).get_child(0)
-	entry_IRL_timestamp.text = cur_date
+	new_entry.set_irl_date(cur_date)
 	entry_box.add_child(new_entry)
 
 
 func _on_RemoveLastEntry_button_up() -> void:
-	if entry_box.get_child_count() > 0:
-		entry_box.get_child(entry_box.get_child_count()-1).queue_free()
+	for entry in entry_box.get_children():
+		if entry.is_selected:
+			entry.queue_free()
 
 # ======== NPC Handling
 
--- src/UI/CharacterSheetSubpages/Journal/JournalEntry.gd
+++ src/UI/CharacterSheetSubpages/Journal/JournalEntry.gd
@@ -1,17 +1,28 @@
-extends HBoxContainer
+extends ColorRect
 class_name JournalEntry
 
 @export var is_empty: bool = true
+@export var is_selected: bool = false
 
 func save():
 	var save_dict = {}
-	save_dict["Timestamp"] = $VBoxContainer/IRLEdit.text
-	save_dict["In Game Timestamp"] = $VBoxContainer/IGEdit.text
-	save_dict["Entry"] = $Entry.text
+	save_dict["Timestamp"] = $HBoxContainer/VBoxContainer/IRLEdit.text
+	save_dict["In Game Timestamp"] = $HBoxContainer/VBoxContainer/IGEdit.text
+	save_dict["Entry"] = $HBoxContainer/Entry.text
 	
 	return save_dict
 	
 func load_sheet(save_dict):
-	$VBoxContainer/IRLEdit.text = save_dict["Timestamp"]
-	$VBoxContainer/IGEdit.text = save_dict["In Game Timestamp"]
-	$Entry.text = save_dict["Entry"]
+	$HBoxContainer/VBoxContainer/IRLEdit.text = save_dict["Timestamp"]
+	$HBoxContainer/VBoxContainer/IGEdit.text = save_dict["In Game Timestamp"]
+	$HBoxContainer/Entry.text = save_dict["Entry"]
+
+func set_irl_date(str: String) -> void:
+	$HBoxContainer/VBoxContainer/IRLEdit.text = str
+
+func _on_selected_toggled(toggled_on: bool) -> void:
+	self.is_selected = toggled_on
+	if toggled_on:
+		self.color.a = 0.3
+	else:
+		self.color.a = 0
